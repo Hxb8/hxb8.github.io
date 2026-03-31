18c616e33fa4f7a4bad9053862f253d988dd3476
Hexbyte <islamamara062@gmail.com>
2026-03-31 21:48:22 +0000

    pass daynamic mock data

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
 
 const app = express();
+// mocking Data
+const blogs = [
+  {
+    title: 'Email Injection Attack',
+    snippet: 'Bug Bounty writeup about an email injection attack'
+  },
+  {
+    title: 'Race condition Bug that break the 1 limit',
+    snippet: 'This bug can lead to a big financial lose'
+  },
+  {
+    title: 'Information Disclosure',
+    snippet: 'A Low level user can enumerate all emails of workspace members'
+  }
+]
 
 // register a view engine (ejs)
 app.set('view engine', 'ejs');
@@ -10,19 +25,19 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res)=> {
-  res.render('index');
+  res.render('index', { title: 'Home', blogs});
 })
 
 app.get('/about', (req, res)=> {
-  res.render('about');
+  res.render('about', {title: 'About'});
 })
 
 app.get('/blogs/create', (req, res)=> {
-  res.render('create');
+  res.render('create', {title: 'Create a New Blog'});
 })
 
 app.use((req, res) => {
-  res.status(404).render('404');
+  res.status(404).render('404', {title: '404'});
 })
 
 
